a6ebe19db71352572843de0e6d137a8d47956f77
WorldCreator46 <96541154+WorldCreator46@users.noreply.github.com>
2023-02-10 17:06:04 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const a = 12;
+const b = 5;
+let myName = "gun";
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log(`hello ${myName}`);
+
+myName = "geon";
+console.log(`hello ${myName}`);
