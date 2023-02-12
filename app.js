fcc641aa1de8afd252bed57d89f1364c97e0207a
WorldCreator46 <96541154+WorldCreator46@users.noreply.github.com>
2023-02-13 06:37:15 +0900

    .

--- app.js
+++ app.js
@@ -1,23 +1,7 @@
 const h1 = document.querySelector(".hello h1");
 
-function handleh1click() {
-  h1.style.color = "red";
+function handleTitleClick() {
+  h1.classList.toggle("active");
 }
 
-function handleMouseEnter() {
-  h1.innerText = "Mouse is here";
-}
-
-function handleMouseLeave() {
-  h1.innerText = "Mouse is gone";
-}
-
-function handleWindowResize() {
-  document.body.style.backgroundColor = "tomato";
-}
-
-h1.addEventListener("click", handleh1click);
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize);
+h1.addEventListener("click", handleTitleClick);
