7d67ba7d591e52a959d17412ebef17b256837741
WorldCreator46 <96541154+WorldCreator46@users.noreply.github.com>
2023-02-14 08:22:06 +0900

    .

--- js/weather.js
+++ js/weather.js
@@ -3,8 +3,16 @@ const API_KEY = "99f70b4eeb9d70b0e24278bef91bb4ee";
 function onGeoOk(p) {
   const lat = p.coords.latitude;
   const lon = p.coords.longitude;
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric&lang=kr`;
   console.log(url);
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].description} / ${data.main.temp}`;
+    });
 }
 function onGeoError() {
   alert("찾을 수 없습니다.");
